2e3513cc7fbd874b7592b921fec84393f7e6a2bb
Andrew Spinks <andrewspinks@gmail.com>
2015-01-07 17:47:26 +1100

    Try out Swift with 2 spaces indent. Fix test MockProvider setup.

--- PactConsumerSwift/MockService.swift
+++ PactConsumerSwift/MockService.swift
@@ -2,35 +2,39 @@ import Foundation
 import Alamofire
 
 public class MockService {
-    private let provider: String
-    private let consumer: String
-    private let url: String
-    private let port: Int
-    private var interactions: [Interaction] = []
-    
-    public init(provider: String, consumer: String, url: String = "http://localhost", port: Int = 1234) {
-        self.provider = provider
-        self.consumer = consumer
-        self.url = url
-        self.port = port
-    }
-    
-    public func given(providerState: String) -> Interaction {
-        var interaction = Interaction().given(providerState)
-        interactions.append(interaction)
-        return interaction
-    }
-    
-    public func uponReceiving(description: String) -> Interaction {
-        var interaction = Interaction().uponReceiving(description)
-        interactions.append(interaction)
-        return interaction
-    }
-    
-    public func run(testFunction: (cleanup: () -> Void) -> Void) {
-        testFunction({() in
-            // cleanup
-        })
-    }
-    
+  private let provider: String
+  private let consumer: String
+  private let url: String
+  private let port: Int
+  private var interactions: [Interaction] = []
+
+  public init(provider: String, consumer: String, url: String = "http://localhost", port: Int = 1234) {
+    self.provider = provider
+    self.consumer = consumer
+    self.url = url
+    self.port = port
+  }
+
+  public func given(providerState: String) -> Interaction {
+    var interaction = Interaction().given(providerState)
+    interactions.append(interaction)
+    return interaction
+  }
+
+  public func uponReceiving(description: String) -> Interaction {
+    var interaction = Interaction().uponReceiving(description)
+    interactions.append(interaction)
+    return interaction
+  }
+
+  public func run(testFunction: (cleanup:() -> Void) -> Void) {
+    testFunction({ () in
+      // cleanup
+    })
+  }
+
+  func clean() {
+
+  }
+
 }
--- PactConsumerSwiftTests/PactTest.swift
+++ PactConsumerSwiftTests/PactTest.swift
@@ -1,24 +1,26 @@
-
 import Quick
 import Nimble
 import PactConsumerSwift
+import Alamofire
 
 class HelloClientSpec: QuickSpec {
-    override func spec() {
-        it("is friendly") {
-            var hello = "nothingHere"
-            var helloProvider = MockService(provider: "Hello Provider", consumer: "Hello Consumer")
+  override func spec() {
+    it("is friendly") {
+      var hello = "nothingHere"
+      var helloProvider = MockService(provider: "Hello Provider", consumer: "Hello Consumer")
 
-            helloProvider.uponReceiving("a request for hello")
+      helloProvider.uponReceiving("a request for hello")
+                   .withRequest(.GET, path: "/sayHello")
+                   .willRespondWith(200, headers: ["Content-Type": "application/json"], body: "{ reply: \"Hello\" }")
 
-            //Run the tests
-            helloProvider.run { (complete) in
-                HelloClient().sayHello { (response) in
-                    hello = response
-                }
-                expect(hello).toEventually(contain("hello"))
-                complete()
-            }
+      //Run the tests
+      helloProvider.run { (complete) in
+        HelloClient().sayHello { (response) in
+          hello = response
         }
+        expect(hello).toEventually(contain("hello"))
+        complete()
+      }
     }
+  }
 }
