2c1f4c65789f6e12ad44e56d804e521a80d18810
Andrew Spinks <andrewspinks@gmail.com>
2015-02-07 15:47:22 +0900

    Remove example projects - have been split out into other git repos.

--- iOS ObjectiveC Example/iOS ObjectiveC Example/HelloClient.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface HelloClient : NSObject
-- (id)initWithBaseUrl:(NSString *)url;
-- (NSString *)sayHello;
-
-- (NSString *)findFriendsByAgeAndChild;
-- (void) unfriend:(void (^)(NSString *response))success failure:(void (^)(NSInteger errorCode))failure;
-@end
--- iOS ObjectiveC Example/iOS ObjectiveC ExampleTests/BlankClass.swift
+++
@@ -1,2 +0,0 @@
-// NB: This needs to be here so that the example links against the Swift library properly. Bug?
-import PactConsumerSwift
