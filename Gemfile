8532c83067bbd120384c4078c5135e1366070c03
Andrew Spinks <andrewspinks@gmail.com>
2016-05-16 19:16:32 +0900

    update dependencies.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'pact-mock_service', '~> 0.5.1'
+gem 'pact-mock_service', '~> 0.8.2'
 gem 'xcpretty'
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@
 mkdir -p "${SRCROOT}/tmp"
 which pact-mock-service
 
-nohup pact-mock-service start --log "${SRCROOT}/tmp/pact.log" --pact-dir "${SRCROOT}/tmp/pacts" -p 1234 > ${SRCROOT}/tmp/nohup.out 2>&1 &
+nohup pact-mock-service start --pact-specification-version 2.0.0 --log "${SRCROOT}/tmp/pact.log" --pact-dir "${SRCROOT}/tmp/pacts" -p 1234
