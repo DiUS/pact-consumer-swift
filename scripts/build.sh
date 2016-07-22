f49fcc3810efe284db66672f0bdf5a4f2d418489
Rajat Vig <rajat.vig@gmail.com>
2016-07-21 23:40:25 -0700

    Use scan to test

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xcodebuild -workspace PactConsumerSwift.xcworkspace -scheme PactConsumerSwift test -destination 'platform=iOS Simulator,OS=9.3,name=iPhone 6' -sdk iphonesimulator | xcpretty -c
