4086e88a35a4a9a473bfe954ceb61f4e20231ff3
Marko Justinek <marko.justinek@gmail.com>
2017-10-02 11:58:20 +1100

    Reduces the number of destinations in TracisCI and reintroduces build.sh script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-fastlane scan
+if [[ -z "${PROJECT_NAME}" ]]; then
+  PROJECT_NAME="PactConsumerSwift.xcodeproj";
+  DESTINATION="OS=11.0,name=iPhone 8";
+  SCHEME="PactConsumerSwift iOS";
+fi
+
+echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;\""
+
+xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+
+echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;\""
+xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
