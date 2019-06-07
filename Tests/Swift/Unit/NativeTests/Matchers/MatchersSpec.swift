b7305a4adfbf4d7ebfc3bb46fe7edbea2ce4b87a
Lucas Lürich <lucas@2denker.de>
2019-06-07 15:35:36 +0200

    adapted and added test for minType rule

--- Tests/Swift/Unit/NativeTests/Matchers/MatchersSpec.swift
+++ Tests/Swift/Unit/NativeTests/Matchers/MatchersSpec.swift
@@ -8,11 +8,10 @@ class NativeMatcherSpec: QuickSpec {
       let regex = "\\d{16}"
       let placeholder = "1111222233334444"
       let subject = NativeMatcher().term(matcher: regex, generate: placeholder) as! MatchingRule
-
       it("rule matches term and contains regex") {
-        expect(subject.rule()).to(equal(
-          ["match": "regex",
-          "regex": regex]
+        expect(subject.rule() as NSDictionary).to(equal(
+            ["match": "regex",
+            "regex": regex] as NSDictionary
         ))
       }
 
@@ -26,13 +25,32 @@ class NativeMatcherSpec: QuickSpec {
       let subject = NativeMatcher().somethingLike(1234) as! MatchingRule
 
       it("has a type rule") {
-        expect(subject.rule()).to(equal(["match": "type"]))
+        expect(subject.rule() as NSDictionary).to(equal(
+            ["match": "type"] as NSDictionary
+        ))
       }
 
       it("has a value") {
         let value = subject.value() as! Int
         expect(value).to(equal(1234))
       }
+    }
+
+    describe("minType matcher") {
+        let example: [String : Any] = ["key1": 5, "key2": "value"]
+        let subject = NativeMatcher().eachLike(example, min: 2) as! MatchingRule
+
+        it("has a minType rule") {
+            expect(subject.rule() as NSDictionary).to(equal(
+                [ "match": "type",
+                "min": 2] as NSDictionary
+            ))
+        }
+
+        it("has example as value") {
+            let value = subject.value() as! NSDictionary
+            expect(value).to(equal(example as NSDictionary))
+        }
     }
   }
 }
