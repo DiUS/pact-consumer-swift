12600ae1f78f336c925ad29a9f927df365e4f923
Lucas Lürich <lucas@2denker.de>
2019-06-07 20:42:18 +0200

    fixed target selection

--- Sources/Supporting Files/PactConsumerSwift.h
+++ Sources/Supporting Files/PactConsumerSwift.h
@@ -15,7 +15,7 @@ FOUNDATION_EXPORT const unsigned char PactConsumerSwift_VersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PactConsumerSwift/PublicHeader.h>
 
-#if os(OSX)
+#if TARGET_OS_OSX
     #import "PactConsumerSwift/pact_mock_server_mac_os.h"
 #else
     #import "PactConsumerSwift/pact_mock_server_ios.h"
