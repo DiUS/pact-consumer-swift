2a4a2c7c0ad36f0625456d8c707709d9ff7989e7
Marko Justinek <marko.justinek@gmail.com>
2019-06-13 17:41:35 +1000

    Resolves outdated SwiftPM packages

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,11 +18,11 @@ echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destinatio
 xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;
 
 # SwiftPM
-#echo "#### Testing DEBUG configuration for SwiftPM compatibility ####"
-#mkdir -p "${SRCROOT}/tmp"
-#pact-mock-service start --pact-specification-version 2.0.0 --log "${SRCROOT}/tmp/pact.log" --pact-dir "${SRCROOT}/tmp/pacts" -p 1234
-#swift build && swift test
+echo "#### Testing DEBUG configuration for SwiftPM compatibility ####"
+mkdir -p "${SRCROOT}/tmp"
+pact-mock-service start --pact-specification-version 2.0.0 --log "${SRCROOT}/tmp/pact.log" --pact-dir "${SRCROOT}/tmp/pacts" -p 1234
+swift build && swift test
 
-#echo "#### Testing RELEASE configuration for SwiftPM compatibility ####"
-#swift build -c release && swift test
-#pact-mock-service stop
+echo "#### Testing RELEASE configuration for SwiftPM compatibility ####"
+swift build -c release && swift test
+pact-mock-service stop
