e49baf3a689e8066171127d0741cdaa3f63d1961
Marko Justinek <marko.justinek@gmail.com>
2019-06-27 21:41:13 +1000

    Remove unnecessary command check

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 mkdir -p "${SRCROOT}/tmp"
-which pact-mock-service
 
 pact-mock-service start --pact-specification-version 2.0.0 --log "${SRCROOT}/tmp/pact.log" --pact-dir "${SRCROOT}/tmp/pacts" -p 1234
