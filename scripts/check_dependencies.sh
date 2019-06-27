57e879185e15063d5795ede1ef359eabb8ab1f6f
Marko Justinek <marko.justinek@gmail.com>
2019-06-27 22:29:10 +1000

    Add script to check fro dependencies

---
+++ scripts/check_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if which pact-mock-service >/dev/null; then
+    echo "Dependencies check:"
+    echo "- pact-mock-service: installed"
+else
+    echo "Dependencies check:" 
+    echo "- pact-mock-service: not found!"
+    echo ""
+    echo "### ERROR ###"
+    echo "pact-mock-service is not installed!"
+    echo "See https://github.com/pact-foundation/pact-ruby-standalone or use Homebrew tap \"pact-foundation/pact-ruby-standalone\""
+    exit 1
+fi
