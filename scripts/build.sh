cc66ee54483f352a6a74908715b769d148b6553d
Andrew Spinks <andrewspinks@gmail.com>
2019-07-03 19:29:27 +0900

    test: Add test around pact mock service to make refactors easier.

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,10 @@ fi
 
 carthage build --no-skip-current --platform $CARTHAGE_PLATFORM
 
+# SwiftPM
+echo "#### Testing DEBUG configuration for SwiftPM compatibility ####"
+swift build
+
 # Carthage - debug
 echo "#### Testing DEBUG configuration for scheme: $SCHEME, with destination: $DESTINATION ####"
 echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;\""
@@ -19,7 +23,3 @@ xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" 
 echo "#### Testing RELEASE configuration for scheme: $SCHEME, with destination: $DESTINATION ####"
 echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;\""
 xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;
-
-# SwiftPM
-echo "#### Testing DEBUG configuration for SwiftPM compatibility ####"
-swift build
