01effeea58cc69416ca1fbb19c2ae0beee928a9e
alan.nichols@outlook.com <alannichols@users.noreply.github.com>
2019-11-12 22:22:16 +0000

    style: add convenience init method to MockService which defaults error reporting but allows configuration of the verifier. (#77)

--- Sources/MockService.swift
+++ Sources/MockService.swift
@@ -22,6 +22,7 @@ open class MockService: NSObject {
   /// - parameter provider: Name of your provider (eg: Calculator API)
   /// - parameter consumer: Name of your consumer (eg: Calculator.app)
   /// - parameter pactVerificationService: Your customised `PactVerificationService`
+  /// - parameter errorReporter: Your customised `ErrorReporter`
   ///
   public init(
     provider: String,
@@ -40,8 +41,24 @@ open class MockService: NSObject {
   ///
   /// - parameter provider: Name of your provider (eg: Calculator API)
   /// - parameter consumer: Name of your consumer (eg: Calculator.app)
+  /// - parameter pactVerificationService: Your customised `PactVerificationService`
   ///
-  /// Use this initialiser to use the default PactVerificationService
+  /// Use this initialiser to use the default XCodeErrorReporter
+  ///
+  public convenience init(provider: String, consumer: String, pactVerificationService: PactVerificationService) {
+    self.init(provider: provider,
+              consumer: consumer,
+              pactVerificationService: pactVerificationService,
+              errorReporter: XCodeErrorReporter())
+  }
+
+  ///
+  /// Convenience Initializer
+  ///
+  /// - parameter provider: Name of your provider (eg: Calculator API)
+  /// - parameter consumer: Name of your consumer (eg: Calculator.app)
+  ///
+  /// Use this initialiser to use the default PactVerificationService and ErrorReporter
   ///
   @objc(initWithProvider: consumer:)
   public convenience init(provider: String, consumer: String) {
