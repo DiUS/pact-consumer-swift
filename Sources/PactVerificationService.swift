21bba0205a33f0d32d7a44e4d67666a42fdc006c
Marko Justinek <marko.justinek@gmail.com>
2020-02-06 09:55:12 +1100

    (fix) Backwards compatibility with Xcode 10.2 and Swift 4.2 (#81)
    * Remove implicit returns
    
    * Lock OHTTPStubs to version 8.0
    
    Version of `OHTTPStubs 9.0` renamed all classes to remove OH.

--- Sources/PactVerificationService.swift
+++ Sources/PactVerificationService.swift
@@ -186,7 +186,7 @@ private extension PactVerificationService {
 private extension PactVerificationService {
 
   var session: URLSession {
-    URLSession(configuration: URLSessionConfiguration.ephemeral)
+    return URLSession(configuration: URLSessionConfiguration.ephemeral)
   }
 
   func performNetworkRequest(for router: Router, completion: @escaping (Result<String, URLSession.APIServiceError>) -> Void) {
@@ -220,15 +220,15 @@ private extension PactVerificationService {
 private extension NSError {
 
   static func prepareWith(userInfo: [String: Any]) -> NSError {
-    NSError(domain: "error", code: 0, userInfo: userInfo)
+    return NSError(domain: "error", code: 0, userInfo: userInfo)
   }
 
   static func prepareWith(message: String) -> NSError {
-    NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("Error", value: message, comment: "")]) //swiftlint:disable:this line_length
+    return NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("Error", value: message, comment: "")]) //swiftlint:disable:this line_length
   }
 
   static func prepareWith(data: Data) -> NSError {
-    NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("Error", value: "\(String(data: data, encoding: .utf8) ?? "Failed to cast response Data into String")", comment: "")]) //swiftlint:disable:this line_length
+    return NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("Error", value: "\(String(data: data, encoding: .utf8) ?? "Failed to cast response Data into String")", comment: "")]) //swiftlint:disable:this line_length
   }
 
 }
