66771dc1c071c337aaa68f5bdd051eea4595ed40
Marko Justinek <marko.justinek@gmail.com>
2020-02-11 07:30:02 +1100

    (fix) Run two `pact-mock-service`s
    - Starts pact-mock-service on port 1234 and another with self signed SSL certificate on port 2345
    
    - Stops both pact-mock-services by searching for the process and killing them

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p "${SRCROOT}/tmp"
-which pact-mock-service
 
-pact-mock-service start --ssl --pact-specification-version 2.0.0 --log "${SRCROOT}/tmp/pact.log" --pact-dir "${SRCROOT}/tmp/pacts" -p 1234
+pact-mock-service start --pact-specification-version 2.0.0 --log "${SRCROOT}/tmp/pact.log" --pact-dir "${SRCROOT}/tmp/pacts" -p 1234
+pact-mock-service start --ssl --pact-specification-version 2.0.0 --log "${SRCROOT}/tmp/pact-ssl.log" --pact-dir "${SRCROOT}/tmp/pacts-ssl" -p 2345
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,4 @@
 
-pact-mock-service stop
+#pact-mock-service stop
+
+ps -ef | grep pact-mock-service | grep -v grep | awk '{print $2}' | xargs kill
