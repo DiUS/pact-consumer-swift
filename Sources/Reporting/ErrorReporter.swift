c70d557a74b269f4773cc7d3a555adcb8c7f8a5d
Ruben <rcagnie@wayfair.com>
2020-04-01 19:50:31 -0400

    Fixed failing travis pipeline

--- Sources/Reporting/ErrorReporter.swift
+++ Sources/Reporting/ErrorReporter.swift
@@ -1,10 +1,6 @@
 import Foundation
 
-#if SWIFT_PACKAGE
 public typealias FileString = StaticString
-#else
-public typealias FileString = String
-#endif
 
 public protocol ErrorReporter {
   func reportFailure(_ message: String)
