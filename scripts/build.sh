7dc4f7ccb7b42c21acd87732ea73f216a2f23d9b
Marko Justinek <marko.justinek@gmail.com>
2020-12-12 15:14:12 +1100

    tech: Move CI to GitHub Actions

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,10 +11,6 @@ fi
 # Build Carthage dependencies
 carthage build --platform $CARTHAGE_PLATFORM
 
-# SwiftPM
-echo "#### Testing DEBUG configuration for SwiftPM compatibility ####"
-swift build
-
 # Carthage - debug
 echo "#### Testing DEBUG configuration for scheme: $SCHEME, with destination: $DESTINATION ####"
 echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcbeautify\""
@@ -24,6 +20,3 @@ set -o pipefail && xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destinat
 echo "#### Testing RELEASE configuration for scheme: $SCHEME, with destination: $DESTINATION ####"
 echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcbeautify\""
 set -o pipefail && xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcbeautify
-
-# Test the lot that it builds for Carthage
-carthage build --no-skip-current --platform $CARTHAGE_PLATFORM
--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+gem install xcpretty
+brew tap thii/xcbeautify https://github.com/thii/xcbeautify.git
+brew install swiftlint xcbeautify
 brew update && brew bundle
 carthage checkout
