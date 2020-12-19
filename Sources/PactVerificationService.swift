99ae05dd6cef7309394d83675759d28c1918234f
Marko Justinek <marko.justinek@gmail.com>
2020-12-20 00:10:00 +1100

    chore: Fix swiftlint complaint

--- Sources/PactVerificationService.swift
+++ Sources/PactVerificationService.swift
@@ -264,11 +264,11 @@ private extension NSError {
   }
 
   static func prepareWith(message: String) -> NSError {
-    return NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("Error", value: message, comment: "")]) //swiftlint:disable:this line_length
+    return NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("Error", value: message, comment: "")]) // swiftlint:disable:this line_length
   }
 
   static func prepareWith(data: Data) -> NSError {
-    return NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("Error", value: "\(String(data: data, encoding: .utf8) ?? "Failed to cast response Data into String")", comment: "")]) //swiftlint:disable:this line_length
+    return NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("Error", value: "\(String(data: data, encoding: .utf8) ?? "Failed to cast response Data into String")", comment: "")]) // swiftlint:disable:this line_length
   }
 
 }
