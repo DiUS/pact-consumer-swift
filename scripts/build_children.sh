3b724b241ab4f874415f764dbe8c3a305c0645d3
Marko Justinek <marko.justinek@gmail.com>
2020-12-22 00:12:31 +1100

    fix: Use username and token over Authorization header

--- scripts/build_children.sh
+++ scripts/build_children.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -eu
+set -xeu
 set -o pipefail
 
-TRAVISCI_AUTH_TOKEN=${AUTH_TOKEN:="invalid"}
-GITHUB_AUTH_TOKEN=${GH_BUILD_CHILDREN_TOKEN:="invalid"}
+TRAVISCI_AUTH_TOKEN=${AUTH_TOKEN:-"invalid_travis_ci_token"}
+GITHUB_AUTH_TOKEN=${GH_BUILD_CHILDREN_TOKEN:-"invalid_github_token"}
 COMMIT_MESSAGE=${COMMIT_MESSAGE:="repository_dispatched"}
 
 function triggerTravisCIBuild {
@@ -13,7 +13,7 @@ function triggerTravisCIBuild {
     -H "Accept: application/json" \
     -H "Travis-API-Version: 3" \
     -H "Authorization: token ${TRAVISCI_AUTH_TOKEN}" \
-    -d "{"request": {"branch":"master"}}" \
+    -d "{\"request\": {\"branch\":\"master\"}}" \
     https://api.travis-ci.org/repo/$1/requests
 }
 
@@ -22,7 +22,7 @@ function triggerGitHubActionsBuild {
 	https://api.github.com/repos/$1/dispatches \
 	-H "Accept: application/vnd.github.everest-preview+json" \
 	-H "Content-Type: application/json" \
-	-H "Authorization: token $GITHUB_AUTH_TOKEN" \
+	-u ${GITHUB_AUTH_TOKEN} \
 	--data "{\"event_type\":\"pact-consumer-swift - ${COMMIT_MESSAGE}\"}"
 }
 
