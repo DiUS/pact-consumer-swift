7acce1ee84b9354b62c9e52a3aa44afe86ccbf3f
Marko Justinek <marko.justinek@gmail.com>
2021-01-07 15:57:05 +1100

    test: Amended without any quotes like ' " : - _

--- scripts/build_children.sh
+++ scripts/build_children.sh
@@ -5,8 +5,8 @@ set -o pipefail
 
 TRAVISCI_AUTH_TOKEN=${AUTH_TOKEN:-"invalid_travis_ci_token"}
 GITHUB_AUTH_TOKEN=${GH_BUILD_CHILDREN_TOKEN:-"invalid_github_token"}
-COMMIT_MESSAGE=${COMMIT_MESSAGE:="repository_dispatched"}
-COMMIT_MESSAGE_FIRST_LINE_ONLY=`echo "${COMMIT_MESSAGE}" | head -1`
+COMMIT_MESSAGE=${COMMIT_MESSAGE:="repository dispatched"} | head -1
+CLEAN_MESSAGE=$(echo "${COMMIT_MESSAGE[0]}" | sed -e 's/[^a-z^A-Z|^_^ ^:^-]//g')
 
 function triggerTravisCIBuild {
   curl -s -X POST --silent --show-error --fail \
@@ -20,18 +20,20 @@ function triggerTravisCIBuild {
 
 function triggerGitHubActionsBuild {
 	curl -X POST --silent --show-error --fail \
-	https://api.github.com/repos/$1/dispatches \
-	-H "Accept: application/vnd.github.everest-preview+json" \
-	-H "Content-Type: application/json" \
-	-u ${GITHUB_AUTH_TOKEN} \
-	--data "{\"event_type\":\"triggered ${COMMIT_MESSAGE_FIRST_LINE_ONLY}\"}"
+		https://api.github.com/repos/$1/dispatches \
+		-H "Accept: application/vnd.github.everest-preview+json" \
+		-H "Content-Type: application/json" \
+		-u ${GITHUB_AUTH_TOKEN} \
+		--data "{\"event_type\":\"triggered ${CLEAN_MESSAGE}\"}"
 }
 
 # GitHub Actions
 # - <name> and <repo> must be lower-case!
+echo "Triggering GitHub Actions"
 triggerGitHubActionsBuild surpher/pactswiftpmexample
 triggerGitHubActionsBuild surpher/pactmacosexample
 
 # TravisCI
+echo "Triggering TravisCI builds"
 triggerTravisCIBuild andrewspinks%2FPactObjectiveCExample
 triggerTravisCIBuild andrewspinks%2FPactSwiftExample
