0a4da62a4f45756abed1301bc1b39ad365c148b6
Marko Justinek <marko.justinek@gmail.com>
2021-01-18 08:40:08 +1100

    fix: Explicitly install tools for macOS 11.0 CI

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-set -e
+set -eu
 
-brew update && brew bundle
-carthage checkout
+# GitHub Hosted runners are complaining about re-installing Swiftlint using brew.
+if [[ "$CI" == true ]]; then
+	# Explicitly install only pact-ruby-standalone and xcbeautify, other tools are already installed on the GitHub hosted runner
+	brew tap 'pact-foundation/pact-ruby-standalone', 'https://github.com/pact-foundation/homebrew-pact-ruby-standalone.git'
+	brew tap 'thii/xcbeautify', 'https://github.com/thii/xcbeautify.git'
+	brew install 'pact-ruby-standalone'
+	brew install 'xcbeautify'
+else
+	# Install dependencies using Brewfile
+	brew update && brew bundle
+	carthage checkout
+fi
