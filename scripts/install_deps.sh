d04ec9d67ceefa6d4f6a1edafd5fc78a84759e1d
Marko Justinek <marko.justinek@gmail.com>
2021-02-03 22:56:35 +1100

    fix: Update install_deps script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -eu
 
 # GitHub Hosted runners are complaining about re-installing Swiftlint using brew.
 if [[ "$CI" == true ]]; then
 	# Explicitly install only pact-ruby-standalone and xcbeautify, other tools are already installed on the GitHub hosted runner
-	brew tap 'pact-foundation/pact-ruby-standalone', 'https://github.com/pact-foundation/homebrew-pact-ruby-standalone.git'
-	brew tap 'thii/xcbeautify', 'https://github.com/thii/xcbeautify.git'
+	brew tap 'pact-foundation/pact-ruby-standalone'
+	brew tap 'thii/xcbeautify'
 	brew install 'pact-ruby-standalone'
 	brew install 'xcbeautify'
 else
