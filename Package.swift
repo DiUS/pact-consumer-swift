7aba867fe342a89a1178c1084be69e8e0bc044c3
alexbasson <alexbasson@gmail.com>
2021-05-13 14:29:01 -0400

    Update Package.swift to set minimum supported iOS version to 9
    This aligns the Package.swift with the Cocoapods manifest, which also specifies a minimum iOS version of 9
    
    This commit resolves issue #122
    (https://github.com/DiUS/pact-consumer-swift/issues/122)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "PactConsumerSwift",
   platforms: [
-    .macOS(.v10_10), .iOS(.v8), .tvOS(.v9)
+    .macOS(.v10_10), .iOS(.v9), .tvOS(.v9)
   ],
   products: [
     .library(name: "PactConsumerSwift", targets: ["PactConsumerSwift"])
